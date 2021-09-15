1310a96922fe71e5f15764057843c9256efc6144
lifachao <2390210930@qq.com>
2021-09-15 18:19:42 +0800

    热模块 修改

--- src/index.js
+++ src/index.js
@@ -4,31 +4,32 @@ import ReactDOM from "react-dom";
 import ClassApp from "./page/ClassApp";
 import FuncAPP from "./page/FuncApp";
 // redux 储存器
-import store from './redux/store/store'
-import { Provider } from 'react-redux';
+import store from "./redux/store/store";
+import { Provider } from "react-redux";
 // 样式
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 import "./index.css";
 
-ReactDOM.render(
+const app = (
   <React.StrictMode>
     <Provider store={store}>
       {/* 类组件使用redux */}
       <ClassApp />
 
       {/* 函数组件使用redux  二者选一*/}
-      {/* <FuncAPP/> */}
+      <FuncAPP />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // 热模块更新
 if (module.hot) {
   // accept 函数的第一个参数指出当前文件接受哪些子模块的替换，这里表示只接受 ./page/ClassApp 这个子模块
   // 第2个参数用于在新的子模块加载完毕后需要执行的逻辑
-  module.hot.accept(['./page/ClassApp'], () => {
+  module.hot.accept(["./page/ClassApp","./page/FuncApp"], () => {
     // 新的 AppComponent 加载成功后重新执行下组建渲染逻辑
-    ReactDOM.render(<ClassApp />, document.getElementById('root'));
+    ReactDOM.render(app, document.getElementById("root"));
   });
 }
